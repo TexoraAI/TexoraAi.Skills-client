b35217744d723048140e2927ab2cb9ca1cb603cb
Raghib Imam <raghibimam0@gmail.com>
2026-03-11 14:46:54 +0530

    updated

--- src/services/batchService.js
+++ src/services/batchService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =   import.meta.env.VITE_API_BASE_URL || "http://localhost:9000/api";
+const BASE_URL =   import.meta.env.VITE_API_BASE_URL/api || "http://localhost:9000/api";
 
 /* ================= AUTH ================= */
 
